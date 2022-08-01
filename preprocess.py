3d4d6ddc07376cb8a1870547a07f1d47ab58ca01
tbohne <tim.bohne@dfki.de>
2022-08-01 15:41:28 +0200

    implement basic data preprocessing

---
+++ preprocess.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+import pandas as pd
+import numpy as np
+
+
+def read_oscilloscope_recording(rec_file):
+    print("reading oscilloscope recording from", rec_file)
+
+    # label: pos (1) / neg (0)
+    label = 1 if "POS" in str(rec_file) else 0
+
+    df = pd.read_csv(rec_file, delimiter=';', na_values=['-∞', '∞'])
+    df = df[1:].apply(lambda x: x.str.replace(',', '.')).astype(float).dropna()
+    curr_voltages = list(df['Kanal A'].values)
+
+    return label, curr_voltages
+
+
+def iterate_through_input_data():
+    labels = []
+    voltages = []
+    for path in Path("data/").glob('**/*.csv'):
+        label, curr_voltages = read_oscilloscope_recording(path)
+        labels.append(label)
+        voltages.append(curr_voltages)
+
+    print(len(voltages))
+    print(np.array(voltages, dtype=object).shape)
+    np.savez("data/training_data.npz", np.array(voltages, dtype=object), np.array(labels))
+
+
+if __name__ == '__main__':
+    # input: raw oscilloscope data (one file per recording)
+    # output: preprocessed data (one training data file containing data of all recordings)
+    iterate_through_input_data()
