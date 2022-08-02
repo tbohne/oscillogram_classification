601e34b71f54bf22c9ea33014baf830ac07abf46
tbohne <tim.bohne@dfki.de>
2022-08-02 10:29:06 +0200

    implement model training

--- train.py
+++ train.py
@@ -6,6 +6,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow import keras
 
+EPOCHS = 500
+BATCH_SIZE = 32
+
 
 def visualize_one_example_per_class(x, y):
     classes = np.unique(y, axis=0)
@@ -41,6 +44,28 @@ def create_model(input_shape, num_classes):
     return keras.models.Model(inputs=input_layer, outputs=output_layer)
 
 
+def train_model(x_train, y_train):
+    callbacks = [
+        keras.callbacks.ModelCheckpoint("best_model.h5", save_best_only=True, monitor="val_loss"),
+        keras.callbacks.ReduceLROnPlateau(monitor="val_loss", factor=0.5, patience=20, min_lr=0.0001),
+        keras.callbacks.EarlyStopping(monitor="val_loss", patience=50, verbose=1),
+    ]
+    model.compile(
+        optimizer="adam",
+        loss="sparse_categorical_crossentropy",
+        metrics=["sparse_categorical_accuracy"],
+    )
+    history = model.fit(
+        x_train,
+        y_train,
+        batch_size=BATCH_SIZE,
+        epochs=EPOCHS,
+        callbacks=callbacks,
+        validation_split=0.2,
+        verbose=1,
+    )
+
+
 if __name__ == '__main__':
     data = TrainingData(np.load("data/training_data.npz", allow_pickle=True))
     print("number of recordings (oscillograms):", len(data))
@@ -57,5 +82,10 @@ if __name__ == '__main__':
     x_train = x_train[idx]
     y_train = y_train[idx]
 
+    x_train = np.asarray(x_train).astype('float32')
+    y_train = np.asarray(y_train).astype('float32')
+
     model = create_model(x_train.shape[1:], len(np.unique(y_train)))
     keras.utils.plot_model(model, to_file="model.png", show_shapes=True)
+
+    train_model(x_train, y_train)
