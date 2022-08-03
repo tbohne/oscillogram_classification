cd383877d32ef37d9ce5879228543633d22b0099
tbohne <tim.bohne@dfki.de>
2022-08-03 10:33:03 +0200

    implement Grad-CAM heatmap vis.

--- grad_cam.py
+++ grad_cam.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @author Tim Bohne
+
+import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
@@ -6,7 +11,6 @@ import preprocess
 
 
 def generate_gradcam_heatmap(input_array, trained_model, last_conv_layer, pred_idx=None):
-
     # model that maps the input to the activations of the last conv layer as well as the output predictions
     grad_model = tf.keras.models.Model(
         [trained_model.inputs], [trained_model.get_layer(last_conv_layer).output, trained_model.output]
@@ -34,6 +38,22 @@ def generate_gradcam_heatmap(input_array, trained_model, last_conv_layer, pred_i
     # for visualization purpose, normalize heatmap between 0 & 1
     heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
     return heatmap.numpy()
+
+
+def plot_heatmap(heatmap):
+    plt.rcParams["figure.figsize"] = 10, 4
+    fig, (ax, ax2) = plt.subplots(nrows=2, sharex=True)
+
+    # bounding box in data coordinates that the image will fill (left, right, bottom, top)
+    extent = [0, heatmap.shape[0], 0, 1]
+
+    ax.imshow(heatmap[np.newaxis, :], cmap="plasma", aspect="auto", extent=extent)
+    ax.set_yticks([])
+    ax.set_xlim(extent[0], extent[1])
+    data_points = [i for i in range(len(voltages))]
+    ax2.plot(data_points, voltages)
+    plt.tight_layout()
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -69,3 +89,5 @@ if __name__ == '__main__':
     heatmap = generate_gradcam_heatmap(np.array([net_input]), model, last_conv_layer_name)
 
     print("heatmap shape:", heatmap.shape)
+
+    plot_heatmap(heatmap)
