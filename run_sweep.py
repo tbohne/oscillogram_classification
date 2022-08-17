149de0ca160c0b295374cb32a9204aee75862d55
awindler <patricia.windler@dfki.de>
2022-08-17 19:48:40 +0200

    .yaml to .py and fixed problem with sweep agent calling train_procedure

--- run_sweep.py
+++ run_sweep.py
@@ -1,26 +1,29 @@
 import wandb
 import os
 import train
-import yaml
+import argparse
 
-from configs import api_key
+from train import file_path
+from configs import api_key, example_sweep_config
 
 TUNING_METHOD = 'random'
 N_RUNS_IN_SWEEP = 3
 
-
 def main():
-    os.environ["WANDB_API_KEY"] = api_key.wandb_api_key
-    try:
-        with open(".\\configs\\example_sweep_config.yaml") as f:
-            hyperparameter_config = yaml.safe_load(f)
-    except FileNotFoundError:
-        print("Could not find config file!")
-        raise
 
-    sweep_config = {'method': TUNING_METHOD, 'parameters': hyperparameter_config}
+    def call_training_procedure(config_dict=None):
+        return train.train_procedure(args.train_path, args.val_path, args.test_path, hyperparameter_config=config_dict)
+
+    parser = argparse.ArgumentParser(description='Hyperparametertunig with Weights&biases sweep')
+    parser.add_argument('--train_path', type=file_path, required=True)
+    parser.add_argument('--val_path', type=file_path, required=True)
+    parser.add_argument('--test_path', type=file_path, required=True)
+    args = parser.parse_args()
+
+    os.environ["WANDB_API_KEY"] = api_key.wandb_api_key
+    sweep_config = {'method': TUNING_METHOD, 'parameters': example_sweep_config.sweep_config}
     sweep_id = wandb.sweep(sweep_config, project="Oscillogram Classification CNN")
-    wandb.agent(sweep_id, train.train_procedure, count=N_RUNS_IN_SWEEP)
+    wandb.agent(sweep_id, call_training_procedure, count=N_RUNS_IN_SWEEP)
 
 
 if __name__ == '__main__':
