f9119871fed2d7e5f6ab6f70e935cd168fb06c46
tbohne <tim.bohne@dfki.de>
2023-03-02 16:30:00 +0100

    refine clustering / several fixes

--- oscillogram_classification/cluster.py
+++ oscillogram_classification/cluster.py
@@ -13,10 +13,13 @@ SEED = 42
 NUMBER_OF_CLUSTERS = 2  # atm just POS and NEG
 
 
-def plot_results(offset, title, clustering):
+def plot_results(offset, title, clustering, x_train, y_train, y_pred):
+    print("#########################################################################################")
+    print("results for", title)
+    print("#########################################################################################")
     for y in range(2):
         plt.subplot(3, 2, y + offset)
-        for x in X_train[y_pred == y]:
+        for x in x_train[y_pred == y]:
             plt.plot(x.ravel(), "k-", alpha=.2)
         plt.plot(clustering.cluster_centers_[y].ravel(), "r-")
         plt.xlim(0, sz)
@@ -27,34 +30,38 @@ def plot_results(offset, title, clustering):
 
 
 np.random.seed(SEED)
-data = TrainingData(np.load("data/synthetic2_0/training_data.npz", allow_pickle=True))
-X_train = (data[:][0])[..., np.newaxis]
+data = TrainingData(np.load("data/MILESTONE_DEMO/training_data.npz", allow_pickle=True))
+x_train = (data[:][0])[..., np.newaxis]
 y_train = (data[:][1])[..., np.newaxis]
-np.random.shuffle(X_train)
-X_train = TimeSeriesScalerMeanVariance().fit_transform(X_train)
+
+idx = np.random.permutation(len(x_train))
+x_train = x_train[idx]
+y_train = y_train[idx]
+
+x_train = TimeSeriesScalerMeanVariance().fit_transform(x_train)
 
 # we need to reduce the length of the TS (due to runtime)
 # TODO: determine feasible size via experiments
-X_train = TimeSeriesResampler(sz=100).fit_transform(X_train)
-sz = X_train.shape[1]
+x_train = TimeSeriesResampler(sz=len(x_train) // 4).fit_transform(x_train)
+sz = x_train.shape[1]
 plt.figure()
 
 print("Euclidean k-means")
-km = TimeSeriesKMeans(n_clusters=NUMBER_OF_CLUSTERS, verbose=True, random_state=SEED)
-y_pred = km.fit_predict(X_train)
-plot_results(1, "Euclidean $k$-means", km)
+km = TimeSeriesKMeans(n_clusters=NUMBER_OF_CLUSTERS, verbose=False, random_state=SEED)
+y_pred = km.fit_predict(x_train)
+plot_results(1, "Euclidean $k$-means", km, x_train, y_train, y_pred)
 
 print("DBA k-means")
 dba_km = TimeSeriesKMeans(
-    n_clusters=NUMBER_OF_CLUSTERS, n_init=2, metric="dtw", verbose=True, max_iter_barycenter=10, random_state=SEED)
-y_pred = dba_km.fit_predict(X_train)
-plot_results(3, "DBA $k$-means", dba_km)
+    n_clusters=NUMBER_OF_CLUSTERS, n_init=2, metric="dtw", verbose=False, max_iter_barycenter=50, random_state=SEED)
+y_pred = dba_km.fit_predict(x_train)
+plot_results(3, "DBA $k$-means", dba_km, x_train, y_train, y_pred)
 
 print("Soft-DTW k-means")
 sdtw_km = TimeSeriesKMeans(
-    n_clusters=NUMBER_OF_CLUSTERS, metric="softdtw", metric_params={"gamma": .01}, verbose=True, random_state=SEED)
-y_pred = sdtw_km.fit_predict(X_train)
-plot_results(5, "Soft-DTW $k$-means", sdtw_km)
+    n_clusters=NUMBER_OF_CLUSTERS, metric="softdtw", metric_params={"gamma": .01}, verbose=False, random_state=SEED)
+y_pred = sdtw_km.fit_predict(x_train)
+plot_results(5, "Soft-DTW $k$-means", sdtw_km, x_train, y_train, y_pred)
 
 plt.tight_layout()
 plt.show()
