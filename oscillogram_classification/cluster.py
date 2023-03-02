896dc2267607a49f7f8014fe36551d528858265b
tbohne <tim.bohne@dfki.de>
2023-03-02 14:17:58 +0100

    implement init version of oscillogram clustering

---
+++ oscillogram_classification/cluster.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @author Tim Bohne
+
+import matplotlib.pyplot as plt
+import numpy as np
+from tslearn.clustering import TimeSeriesKMeans
+from tslearn.preprocessing import TimeSeriesScalerMeanVariance, TimeSeriesResampler
+
+from training_data import TrainingData
+
+SEED = 42
+NUMBER_OF_CLUSTERS = 2  # atm just POS and NEG
+
+
+def plot_results(offset, title, clustering):
+    for y in range(2):
+        plt.subplot(3, 2, y + offset)
+        for x in X_train[y_pred == y]:
+            plt.plot(x.ravel(), "k-", alpha=.2)
+        plt.plot(clustering.cluster_centers_[y].ravel(), "r-")
+        plt.xlim(0, sz)
+        plt.ylim(-4, 4)
+        plt.text(0.55, 0.85, 'Cluster %d' % y, transform=plt.gca().transAxes)
+        if y == 0:
+            plt.title(title)
+
+
+np.random.seed(SEED)
+data = TrainingData(np.load("data/synthetic2_0/training_data.npz", allow_pickle=True))
+X_train = (data[:][0])[..., np.newaxis]
+y_train = (data[:][1])[..., np.newaxis]
+np.random.shuffle(X_train)
+X_train = TimeSeriesScalerMeanVariance().fit_transform(X_train)
+
+# we need to reduce the length of the TS (due to runtime)
+# TODO: determine feasible size via experiments
+X_train = TimeSeriesResampler(sz=100).fit_transform(X_train)
+sz = X_train.shape[1]
+plt.figure()
+
+print("Euclidean k-means")
+km = TimeSeriesKMeans(n_clusters=NUMBER_OF_CLUSTERS, verbose=True, random_state=SEED)
+y_pred = km.fit_predict(X_train)
+plot_results(1, "Euclidean $k$-means", km)
+
+print("DBA k-means")
+dba_km = TimeSeriesKMeans(
+    n_clusters=NUMBER_OF_CLUSTERS, n_init=2, metric="dtw", verbose=True, max_iter_barycenter=10, random_state=SEED)
+y_pred = dba_km.fit_predict(X_train)
+plot_results(3, "DBA $k$-means", dba_km)
+
+print("Soft-DTW k-means")
+sdtw_km = TimeSeriesKMeans(
+    n_clusters=NUMBER_OF_CLUSTERS, metric="softdtw", metric_params={"gamma": .01}, verbose=True, random_state=SEED)
+y_pred = sdtw_km.fit_predict(X_train)
+plot_results(5, "Soft-DTW $k$-means", sdtw_km)
+
+plt.tight_layout()
+plt.show()
