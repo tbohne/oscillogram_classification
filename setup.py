984adc9ecb214acacea708af9206b0e0b513d8c7
tbohne <tim.bohne@dfki.de>
2025-03-07 15:33:07 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-__version__ = '0.1.9'
+__version__ = '0.2.0'
 URL = 'https://github.com/tbohne/oscillogram_classification'
 
 with open('requirements.txt') as f:
